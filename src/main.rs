cc7726399457ff3684ae4148bb4d1dc0fef88d44
yuchanns <me@yuchanns.xyz>
2023-12-05 23:19:13 +0800

    feat(hypr): add his socket2 event watcher
    Signed-off-by: Hanchin Hsieh <me@yuchanns.xyz>

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::env::{self, temp_dir};
+
+use anyhow::Result;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    net::UnixStream,
+};
+use tracing::debug;
+
+#[derive(Debug)]
+pub struct Event {
+    pub name: String,
+    pub data: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let bind_path = temp_dir().join(format!(
+        "hypr/{}/.socket2.sock",
+        env::var("HYPRLAND_INSTANCE_SIGNATURE")?
+    ));
+    debug!("hyprland socket2 path: {:?}", bind_path);
+    let stream = UnixStream::connect(bind_path).await?;
+    let mut reader = BufReader::new(stream);
+    loop {
+        let mut data = Vec::new();
+        // IPC events list: https://wiki.hyprland.org/hyprland-wiki/pages/IPC/
+        reader.read_until(b'\n', &mut data).await?;
+        data.pop();
+        let data = String::from_utf8(data)?;
+        let parts: Vec<&str> = data.splitn(2, ">>").collect();
+        let event = Event {
+            name: parts[0].to_string(),
+            data: parts[1].to_string(),
+        };
+        debug!("{:?}", event);
+    }
+    // Ok(())
+}
